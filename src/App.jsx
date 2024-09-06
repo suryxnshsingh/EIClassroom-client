db358748e94ea9d6339d44956af9edddac7a70bf
suryxnshsingh <suryxnshsingh@gmail.com>
2024-09-07 03:00:32 +0530

    added about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import BackgroundVideo from './assets/components/land'
 import Card from './assets/components/facultyCard'
+import About from './assets/components/about'
 function App() {
 
   return (
       <div>
         <BackgroundVideo/>
+        <About/>
         <Card/>
       </div>
   )
