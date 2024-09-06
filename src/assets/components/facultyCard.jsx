af3d51ec2536dfef69b22fb1990999983fde1ba8
pranshu0604 <135706895+pranshu0604@users.noreply.github.com>
2024-09-07 04:04:09 +0530

    poppins

--- src/assets/components/facultyCard.jsx
+++ src/assets/components/facultyCard.jsx
@@ -19,15 +19,15 @@ export default function Card({
             />
             <div className="absolute inset-0 flex flex-col justify-between p-4 bg-gradient-to-t from-black to-transparent transition-all duration-300">
                 <div className="transform translate-y-[480%] transition-transform duration-300 group-hover:translate-y-[200%] ml-2">
-                    <h2 className="text-white text-2xl font-bold">{name}</h2>
-                    <p className="text-white text-lg">{designation}</p>
+                    <h2 className="text-white text-2xl poppins-bold">{name}</h2>
+                    <p className="text-white text-lg poppins-semibold">{designation}</p>
                 </div>
                 <div className="space-y-2 opacity-0 transition-opacity duration-300 group-hover:opacity-100 mb-2 ml-2">
-                    <p className="text-white">Employee ID: {employeeId}</p>
-                    <p className="text-white">Qualification: {qualification}</p>
-                    <p className="text-white">Joining Date: {joining}</p>
-                    <p className="text-white">Phone: {phone}</p>
-                    <p className="text-white">Email: {email}</p>
+                    <p className="text-white poppins-medium">Employee ID: {employeeId}</p>
+                    <p className="text-white poppins-medium">Qualification: {qualification}</p>
+                    <p className="text-white poppins-medium">Joining Date: {joining}</p>
+                    <p className="text-white poppins-medium">Phone: {phone}</p>
+                    <p className="text-white poppins-medium">Email: {email}</p>
                 </div>
             </div>
         </div>
