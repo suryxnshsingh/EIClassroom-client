636f118d7249faaaeff098a7397ef8397d0c64d2
suryxnshsingh <suryxnshsingh@gmail.com>
2024-09-07 04:28:10 +0530

    responsive faculty sectionn

--- src/assets/components/faculty.jsx
+++ src/assets/components/faculty.jsx
@@ -17,7 +17,7 @@ export default function Faculty(){
                 <Card name={"Bindu Solanki"} designation={"HOD"} employeeId={"EIC/2022/007"} qualification={"B.Tech"} joining={"2022-09-01"} phone={"+91 1234567890"} email={"XJ9L8@example.com"} imgsrc={"/pranshu.jpg"}/>
                 <Card name={"Girish Soni"} designation={"HOD"} employeeId={"EIC/2022/007"} qualification={"B.Tech"} joining={"2022-09-01"} phone={"+91 1234567890"} email={"XJ9L8@example.com"} imgsrc={"/pranshu.jpg"}/>
                 <Card name={"Keerti Bhargawa"} designation={"HOD"} employeeId={"EIC/2022/007"} qualification={"B.Tech"} joining={"2022-09-01"} phone={"+91 1234567890"} email={"XJ9L8@example.com"} imgsrc={"/pranshu.jpg"}/>
-
+                <Card name={"Bindu Solanki"} designation={"HOD"} employeeId={"EIC/2022/007"} qualification={"B.Tech"} joining={"2022-09-01"} phone={"+91 1234567890"} email={"XJ9L8@example.com"} imgsrc={"/pranshu.jpg"}/>
             </div>
         </div>
     )
