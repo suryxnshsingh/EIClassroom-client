470505a3371da0d2dd605e11ece9f49a44fcb82e
Suryansh Singh <141821325+suryxnshsingh@users.noreply.github.com>
2024-10-03 23:09:43 +0530

    fix

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -3,27 +3,27 @@ import { LinkPreview } from "./linkpreview";
 export default function Footer(){
     return(
         <div>
-            <footer class="bg-white dark:bg-neutral-900">
+            <footer class="bg-neutral-900">
                 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div class="md:flex md:justify-between">
                     <div class="mb-6 md:mb-0 w-28 flex items-center">
                         <img
                             src="/GS LOGO.png" alt="logo"></img>
                         <div className="ml-4">
-                            <div className="text-2xl text-white text-nowrap max-sm:hidden">
-                            EI Classroom Portal
+                            <div className="text-2xl text-white text-nowrap">
+                                EI Classroom Portal
                                 </div>
-                                <div className="text-xl text-white font-thin text-nowrap max-sm:hidden">
-                            Shri G. S. Institute of Tech. and Science
+                                <div className="text-xl text-white font-thin text-nowrap">
+                                    Shri G. S. Institute of Tech. and Science
                                 </div>
-                                <div className="text-xl text-white font-thin text-nowrap max-sm:hidden">
-                            Park Road, Indore
+                                <div className="text-xl text-white font-thin text-nowrap ">
+                                    Park Road, Indore
                                 </div>
                             </div>
                     </div>
                     <div class="grid grid-cols-2 gap-4 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                            <h2 class="mb-6 text-sm font-semibold  uppercase text-white">Resources</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
                                     <a href="/" class="hover:underline">Notes</a>
@@ -34,8 +34,8 @@ export default function Footer(){
                             </ul>
                         </div>
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contact Us</h2>
-                            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                            <h2 class="mb-6 text-sm font-semibold uppercase text-white">Contact Us</h2>
+                            <ul class="text-gray-400 font-medium">
                                 <li class="mb-4">
                                     <a href="#" class="hover:underline">+91 987654321</a>
                                 </li>
@@ -46,7 +46,7 @@ export default function Footer(){
                         </div>
                     </div>
                 </div>
-                <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <hr class="my-6  sm:mx-auto border-gray-700 lg:my-8" />
                 <div class="flex justify-center">
                         <div class="text-sm text-white sm:text-center">
                             Crafted by :
